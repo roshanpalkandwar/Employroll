15b99e1c196e58387b8f5d2824e4d4afaa94e938
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-25 09:34:09 +0530

    [roshan]welecome msg

---
+++ Employeepayroll/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employeepayroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
