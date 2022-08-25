e662a8886bcd8875445cb79ce508cc46b09b6c62
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-08-25 15:54:52 +0530

    [roshan]employeecheck

--- Employeepayroll/Program.cs
+++ Employeepayroll/Program.cs
@@ -2,9 +2,28 @@
 {
     internal class Program
     {
+    
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("welecome to employee pay roll");
+            Console.WriteLine("choose option\n 1.check absent present");
+            int opt = Convert.ToInt32(Console.ReadLine());
+            
+            switch(opt)
+            {
+                case 1:
+                    employeePresentAbsent.emplyeeattendcecheck();
+                    break;
+                default:
+                    Console.WriteLine("enter the wrong choice");
+                    break;
+            }
+        
+        
         }
+
+
+
     }
 }
---
+++ Employeepayroll/employeePresentAbsent.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Employeepayroll
+{
+    internal class employeePresentAbsent
+    {
+        public static void emplyeeattendcecheck()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            Console.WriteLine("random genrate value " + empcheck);
+            if (empcheck == 0)
+            {
+                Console.WriteLine("emplyee present");
+
+            }
+            else
+            {
+                Console.WriteLine("employee absent");
+            }
+
+        }
+    }
+}
